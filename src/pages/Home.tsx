acb449c5fcc0225a868d11302749c0b23cc9fbc4
Aleksandr232 <aleksrowing232@gmail.com>
2022-07-19 23:08:36 +0300

    api

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,8 @@
 import React from "react";
+import {useSearchUsersQuery} from '../store/github/github.api'
 
 export function Home(){
+    const {isLoading, isError}=useSearchUsersQuery('Aleksandr232')
     return(
         <div>ум</div>
     )
--- src/store/github/github.api.ts
+++ src/store/github/github.api.ts
@@ -1,3 +1,20 @@
-import {createApi} from '@reduxjs/toolkit/query'
+import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
 
-export const githubApi = createApi
+export const githubApi = createApi({
+    reducerPath: 'gihub/api',
+    baseQuery: fetchBaseQuery({
+        baseUrl: 'https://api.github.com/'
+    }),
+    endpoints: build=>({
+        searchUsers: build.query<any, string>({
+            query: (search: string)=>({
+                url:`search/users`,
+                params:{
+                    q:search
+                }
+            })
+        })
+    })
+})
+    
+export const {useSearchUsersQuery} = githubApi
