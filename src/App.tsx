697bb67936a62045e545c964e30b89cb6e0f8272
Aleksandrgit config --global user.email  aleksrowing232@gmail.com <aleksrowing232@gmail.com>
2022-07-20 22:14:41 +0300

    app

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ function App() {
       <Navigation/>
         <Routes>
             <Route path='/' element={<Home/>}/>
-            <Route path='/favorites' element={<Favorites/>}/>
+            <Route path='/favorites' elemen={<Favorites/>}/>
         </Routes>
     </>
   );
